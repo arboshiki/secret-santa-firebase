ddc14374745dca8adf8d5fa6a0298a1db599c49b
koco <keshikashviligio@gmail.com>
2016-12-12 15:44:04 +0400

    added fb login

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -9,10 +9,23 @@
   function LoginController(Auth,$log) {
     var vm = this;
     vm.loginWithGoogle = loginWithGoogle;
+    vm.loginWithFacebook = loginWithFacebook;
 
 
     function loginWithGoogle() {
       var provider = new firebase.auth.GoogleAuthProvider();
+
+      Auth
+        .$signInWithPopup(provider)
+        .then(function successCallback() {
+          $log.debug("Sign in with popup callback", arguments);
+        })
+        .catch(function errorCallback(error) {
+          vm.errorMessage = error.message;
+        });
+    }
+    function loginWithFacebook() {
+      var provider = new firebase.auth.FacebookAuthProvider();
 
       Auth
         .$signInWithPopup(provider)
